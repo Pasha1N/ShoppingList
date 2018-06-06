ecc397e30520297f323dfee72b95353bd1ca662b
p a s h a <startxx@hotmail.com>
2018-06-06 21:58:30 +0300

    1

--- ShoppingList/Form1.Designer.cs
+++ ShoppingList/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ShoppingList
 {
-    partial class Form1
+    partial class windowShoppingList
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.shoppingList = new System.Windows.Forms.CheckedListBox();
+            this.add = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.item = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // shoppingList
+            // 
+            this.shoppingList.FormattingEnabled = true;
+            this.shoppingList.Location = new System.Drawing.Point(12, 12);
+            this.shoppingList.Name = "shoppingList";
+            this.shoppingList.Size = new System.Drawing.Size(343, 349);
+            this.shoppingList.TabIndex = 0;
+            this.shoppingList.SelectedIndexChanged += new System.EventHandler(this.shoppingList_SelectedIndexChanged);
+            // 
+            // add
+            // 
+            this.add.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.add.Enabled = false;
+            this.add.Location = new System.Drawing.Point(280, 402);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 20);
+            this.add.TabIndex = 1;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // delete
+            // 
+            this.delete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.delete.Enabled = false;
+            this.delete.Location = new System.Drawing.Point(12, 431);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(346, 23);
+            this.delete.TabIndex = 2;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // item
+            // 
+            this.item.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.item.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.item.Location = new System.Drawing.Point(12, 402);
+            this.item.Name = "item";
+            this.item.Size = new System.Drawing.Size(262, 20);
+            this.item.TabIndex = 3;
+            this.item.Text = "  ";
+            this.item.TextChanged += new System.EventHandler(this.item_TextChanged);
+            // 
+            // windowShoppingList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(367, 482);
+            this.Controls.Add(this.item);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.shoppingList);
+            this.Name = "windowShoppingList";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shopping List";
+            this.Load += new System.EventHandler(this.gh_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox shoppingList;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.TextBox item;
     }
 }
 
--- ShoppingList/Form1.cs
+++ ShoppingList/Form1.cs
@@ -10,11 +10,78 @@ using System.Windows.Forms;
 
 namespace ShoppingList
 {
-    public partial class Form1 : Form
+    public partial class windowShoppingList : Form
     {
-        public Form1()
+        public windowShoppingList()
         {
             InitializeComponent();
+        }
+
+        private void item_TextChanged(object sender, EventArgs e)
+        {
+            if (item.Text.Length == 0)
+            {
+                add.Enabled = false;
+            }
+
+            for (int i = 0; i < item.Text.Length; i++)
+            {
+                if (!char.IsSeparator(item.Text[i]))
+                {
+                    add.Enabled = true;
+                    break;
+                }
+                else
+                {
+                    add.Enabled = false;
+                }
+            }
+
+            string copyItem = item.Text;
+            bool work = true;
+            copyItem = copyItem.Trim();
+            
+                while (work)
+                {
+                    copyItem = copyItem.Replace("  ", " ");
+
+                    if (copyItem.IndexOf("  ") == -1)
+                    {
+                        work = false;
+                    }
+
+
+                   
+                }
+            
+            copyItem = null;
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            item.Text = item.Text.Trim();
+
+            shoppingList.Items.Add(item.Text);
+            add.Enabled = false;
+            item.Text = null;
+            
+        }
+
+        private void shoppingList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            delete.Enabled = true;
+        }
+
+
+        private void gh_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            shoppingList.Items.RemoveAt(shoppingList.SelectedIndex);
+            delete.Enabled = false;
         }
     }
 }
