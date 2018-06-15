aa44ffb259968bb962990f71d090fb7e7a52e702
p a s h a <startxx@hotmail.com>
2018-06-15 15:16:33 +0300

    2

---
+++ ShoppingList/ShoppingList.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace ShoppingList
+{
+    partial class windowShoppingList
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shoppingList = new System.Windows.Forms.CheckedListBox();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.item = new System.Windows.Forms.TextBox();
+            this.delete = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // shoppingList
+            // 
+            this.shoppingList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.shoppingList.FormattingEnabled = true;
+            this.shoppingList.Location = new System.Drawing.Point(12, 12);
+            this.shoppingList.Name = "shoppingList";
+            this.shoppingList.Size = new System.Drawing.Size(594, 349);
+            this.shoppingList.TabIndex = 0;
+            this.shoppingList.SelectedIndexChanged += new System.EventHandler(this.shoppingList_SelectedIndexChanged);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUp.Enabled = false;
+            this.buttonUp.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonUp.Location = new System.Drawing.Point(531, 367);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(75, 20);
+            this.buttonUp.TabIndex = 10;
+            this.buttonUp.Text = "Up";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDown.Enabled = false;
+            this.buttonDown.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonDown.Location = new System.Drawing.Point(531, 394);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(75, 20);
+            this.buttonDown.TabIndex = 9;
+            this.buttonDown.Text = "Down";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // item
+            // 
+            this.item.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.item.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.item.Location = new System.Drawing.Point(12, 367);
+            this.item.Name = "item";
+            this.item.Size = new System.Drawing.Size(432, 20);
+            this.item.TabIndex = 8;
+            this.item.Text = "  ";
+            this.item.TextChanged += new System.EventHandler(this.item_TextChanged);
+            // 
+            // delete
+            // 
+            this.delete.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.delete.Enabled = false;
+            this.delete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.delete.Location = new System.Drawing.Point(12, 393);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(513, 21);
+            this.delete.TabIndex = 7;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // add
+            // 
+            this.add.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.add.Enabled = false;
+            this.add.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.add.Location = new System.Drawing.Point(450, 367);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 20);
+            this.add.TabIndex = 6;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // windowShoppingList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 426);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.item);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.shoppingList);
+            this.Name = "windowShoppingList";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shopping List";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox shoppingList;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.TextBox item;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button add;
+    }
+}
+
---
+++ ShoppingList/ShoppingList.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ShoppingList
+{
+    public partial class windowShoppingList : Form
+    {
+        public windowShoppingList()
+        {
+            InitializeComponent();
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            item.Text = item.Text.Trim();
+            shoppingList.Items.Add(item.Text);
+            add.Enabled = false;
+            item.Text = null;
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            shoppingList.Items.Insert(shoppingList.SelectedIndex + 2, shoppingList.Items[shoppingList.SelectedIndex]);
+            shoppingList.SetSelected(shoppingList.SelectedIndex + 2, true);
+            shoppingList.Items.RemoveAt(shoppingList.SelectedIndex - 2);
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            shoppingList.Items.Insert(shoppingList.SelectedIndex - 1, shoppingList.Items[shoppingList.SelectedIndex]);
+            shoppingList.SetSelected(shoppingList.SelectedIndex - 2, true);
+            shoppingList.Items.RemoveAt(shoppingList.SelectedIndex + 2);
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            shoppingList.Items.RemoveAt(shoppingList.SelectedIndex);
+            delete.Enabled = false;
+            buttonUp.Enabled = false;
+            buttonDown.Enabled = false;
+        }
+
+        private void item_TextChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < item.Text.Length; i++)
+            {
+                if (!char.IsSeparator(item.Text[i]))
+                {
+                    add.Enabled = true;
+                    break;
+                }
+                else
+                {
+                    add.Enabled = false;
+                }
+            }
+
+            string copyItem = item.Text;
+            copyItem = copyItem.Trim();
+
+            foreach (object element in shoppingList.Items)
+            {
+                string copyElement = element.ToString();
+
+                if (copyElement == copyItem)
+                {
+                    add.Enabled = false;
+                    break;
+                }
+                else
+                {
+                    add.Enabled = true;
+                }
+            }
+
+            if (item.Text.Length == 0)
+            {
+                add.Enabled = false;
+            }
+        }
+
+        private void shoppingList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            delete.Enabled = true;
+            buttonUp.Enabled = shoppingList.SelectedIndex > 0 ? true : false;
+            buttonDown.Enabled = shoppingList.SelectedIndex < shoppingList.Items.Count - 1 ? true : false;
+        }
+    }
+}
